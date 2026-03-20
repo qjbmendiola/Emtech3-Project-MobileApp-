986dcf20b22ec9dbeba3e8b6190ba5ba3570627a
June Mendiola <qjbmendiola@tip.edu.ph>
2026-03-20 16:30:03 +0800

    Full project (mobile + server)

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
